104af719b369e414a9638ed01dab61e9231aff3c
Artyom Zaporozhets <kraysent@gmail.com>
2022-05-21 12:13:32 +0300

    buttons and field names are now on one level

--- objects_processing.js
+++ objects_processing.js
@@ -99,6 +99,44 @@ function processArrayType(obj) {
     return divBlock
 }
 
+function processFieldName(name, type, parentDivBlock) {
+    let divBlock = document.createElement("div")
+    divBlock.className = "field_name_block"
+
+    let expandButton = document.createElement("button")
+    expandButton.innerHTML = "^"
+    expandButton.className = "expand_btn"
+
+    let currField = document.createElement("text")
+    currField.className = "schema_field_name"
+    currField.innerHTML = `<b>${name}</b>`
+
+    let currType = document.createElement("text")
+    currType.className = "schema_field_type"
+    currType.innerHTML = `: ${type}`
+
+    expandButton.onclick = function () {
+        let children = parentDivBlock.children
+
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].className != "field_name_block") {
+                if (children[i].style.display == "none") {
+                    children[i].style.display = "block"
+                }
+                else {
+                    children[i].style.display = "none"
+                }
+            }
+        }
+    }
+
+    divBlock.appendChild(currField)
+    divBlock.appendChild(currType)
+    divBlock.appendChild(expandButton)
+
+    return divBlock
+}
+
 function processObjectType(obj) {
     let divBlock = document.createElement("div")
     divBlock.className = "schema_field"
@@ -108,42 +146,18 @@ function processObjectType(obj) {
             let curr = obj["properties"][key]
 
             let currDivBlock = document.createElement("div")
-
-            let expandButton = document.createElement("button")
-            expandButton.innerHTML = "^"
-            expandButton.className = "expand_btn"
-
-            let currField = document.createElement("text")
-            currField.className = "schema_field_name"
-            currField.innerHTML = `<b>${key}</b>`
-
-            expandButton.onclick = function () {
-                let children = currDivBlock.children
-
-                for (var i = 0; i < children.length; i++) {
-                    if (children[i].className != "schema_field_name" && children[i].className != "expand_btn") {
-                        if (children[i].style.display == "none") {
-                            children[i].style.display = "block"
-                        }
-                        else {
-                            children[i].style.display = "none"
-                        }
-                    }
-                }
-            }
-
+            let fieldNameDivBlock = processFieldName(key, curr["type"], currDivBlock)
             let currDesc = null
 
             if ("description" in curr) {
                 currDesc = document.createElement("text")
                 currDesc.className = "schema_field_description"
-                currDesc.innerHTML = `[${curr["type"]}] ${curr["description"]}`
+                currDesc.innerHTML = `${curr["description"]}`
             }
 
             let objectBlock = processObjectByType(curr)
 
-            currDivBlock.appendChild(currField)
-            currDivBlock.appendChild(expandButton)
+            currDivBlock.appendChild(fieldNameDivBlock)
 
             if (currDesc != null) {
                 currDivBlock.appendChild(currDesc)
